7d690e228107ecc5dbe0eae6089964c0877bb5d5
DimyLeo <leonardoprimoc@hotmail.com>
2022-11-23 14:54:12 -0300

    Req 18

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,50 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+
+class MatchesModel extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+MatchesModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+MatchesModel.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+MatchesModel.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default MatchesModel;

0c96f0995e74c5a756e75bdd5c1ce3ef4527b2c1
DimyLeo <leonardoprimoc@hotmail.com>
2022-11-25 17:50:16 -0300

    Correção req 19

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,11 +1,15 @@
 import MatchesModel from '../database/models/MatchesModel';
 import IMatches from '../interfaces/IMatches';
 
-export default class TeamsService {
-  public model = MatchesModel;
+export default class MatchesService {
+  constructor(private model = MatchesModel) {}
 
   public async getAll(): Promise<IMatches[]> {
-    const response = await this.model.findAll();
+    const response = await this.model.findAll({
+      include: {
+        all: true,
+        attributes: { exclude: ['id'] } },
+    });
     return response;
   }
 }

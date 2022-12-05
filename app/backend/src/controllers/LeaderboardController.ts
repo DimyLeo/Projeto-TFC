f3f57452439bc9803a0daef6b490479104f15f78
DimyLeo <leonardoprimoc@hotmail.com>
2022-12-05 15:02:46 -0300

    correção away

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -3,12 +3,12 @@ import LeaderboardService from '../services/LeaderboardService';
 
 export default class LeaderboardController {
   public static async getHomeLeaders(req: Request, res: Response) {
-    const response = await LeaderboardService.getHomeMatches();
+    const response = await LeaderboardService.getMatches('teamHome');
     res.status(200).json(response);
   }
 
   public static async getAwayLeaders(req: Request, res: Response) {
-    const response = await LeaderboardService.getAwayMatches();
+    const response = await LeaderboardService.getMatches('teamAway');
     res.status(200).json(response);
   }
 }
--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,6 +1,7 @@
 import MatchesModel from '../database/models/MatchesModel';
 import ILeaderboard from '../interfaces/ILeaderboard';
 import LeaderboardValidate from '../middlewares/leaderboardValidate';
+import SortType from '../middlewares/SortType';
 
 export default class LeaderboardService {
   private static getAllTeams(allMatches: MatchesModel[]): string[] {
@@ -14,34 +15,47 @@ export default class LeaderboardService {
       || (b.goalsOwn - a.goalsOwn));
   }
 
-  private static sortByTeam(allMatches: MatchesModel[], teams: string[]): MatchesModel[][] {
+  private static sortByTeam(
+    allMatches: MatchesModel[],
+    teams: string[],
+    sortType: string,
+  ): MatchesModel[][] {
     return teams.map((team) => allMatches
-      .filter((match) => match.dataValues.teamHome.teamName === team));
+      .filter((match) => match.dataValues[sortType].teamName === team));
   }
 
-  private static async getAllMatches(): Promise<MatchesModel[][]> {
+  private static async getAllMatches(sortType: SortType): Promise<MatchesModel[][]> {
     const response = await MatchesModel.findAll({
       where: { inProgress: false },
       include: { all: true },
     });
     const allTeams = LeaderboardService.getAllTeams(response);
-    const sorteds = LeaderboardService.sortByTeam(response, allTeams);
+    const sorteds = LeaderboardService.sortByTeam(response, allTeams, sortType);
     return sorteds;
   }
 
-  public static async getHomeMatches(): Promise<ILeaderboard[]> {
-    const response = await LeaderboardService.getAllMatches();
-    const homeMatches = response.map((index) =>
-      new LeaderboardValidate(index, index[0].dataValues.teamHome.teamName).leaderboard);
-    const order = LeaderboardService.sort(homeMatches);
-    return order;
+  public static async getMatches(sortType: SortType): Promise<ILeaderboard[]> {
+    const allMatches = await LeaderboardService.getAllMatches(sortType);
+    const matches = allMatches.map((index) => (
+      new LeaderboardValidate(index, index[0].dataValues[sortType].teamName).leaderboard
+    ));
+    const orderedMatches = LeaderboardService.sort(matches);
+    return orderedMatches;
   }
 
-  public static async getAwayMatches(): Promise<ILeaderboard[]> {
-    const response = await LeaderboardService.getAllMatches();
-    const awayMatches = response.map((index) =>
-      new LeaderboardValidate(index, index[0].dataValues.teamAway.teamName).leaderboard);
-    const order = LeaderboardService.sort(awayMatches);
-    return order;
-  }
+  // public static async getHomeMatches(): Promise<ILeaderboard[]> {
+  //   const response = await LeaderboardService.getAllMatches();
+  //   const homeMatches = response.map((index) =>
+  //     new LeaderboardValidate(index, index[0].dataValues.teamHome.teamName).leaderboard);
+  //   const order = LeaderboardService.sort(homeMatches);
+  //   return order;
+  // }
+
+  // public static async getAwayMatches(): Promise<ILeaderboard[]> {
+  //   const response = await LeaderboardService.getAllMatches();
+  //   const awayMatches = response.map((index) =>
+  //     new LeaderboardValidate(index, index[0].dataValues.teamAway.teamName).leaderboard);
+  //   const order = LeaderboardService.sort(awayMatches);
+  //   return order;
+  // }
 }
